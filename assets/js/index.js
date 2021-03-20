358e718c1e74e5bb975886168327d2299fdb2091
nandemotoken <nandemotoken@gmail.com>
2021-03-20 06:21:07 +0000

    gas free version

--- assets/js/index.js
+++ assets/js/index.js
@@ -38,7 +38,7 @@ async function loadmm_matic(){
         window.alert("トランザクションを送信しています。20秒程度お待ちください");
         filter = replica_contract.filters.Transfer(null, add);
         replica_contract.on(filter, (from, to, amount, event) => {
-        ans = window.confirm("OpenSeaでNFTレプリカを確認しましょう\n参考：https://opensea.io/account\n\nOpenSeaを開き、MetaMaskを接続しますか？\n(反映には数分時間がかかることがあります)");
+        ans = window.confirm("OpenSeaでNFTレプリカを確認しましょう\n参考：https://opensea.io/account\n\nOpenSeaを開き、MetaMaskを接続しますか？\n(画像の反映などには数分時間がかかることがあります)");
         if(ans){ window.open("https://opensea.io/account"); }
 
     });
@@ -49,6 +49,32 @@ async function loadmm_matic(){
 
 async function loadmm_gasfree(){
     $('#wallet-popup').modal('hide');
-    window.alert("開発中です。しばらくお待ちください");
+    if (typeof web3 == 'undefined'){
+        window.alert("metamaskをインストールしてください");
+        return;
+    }
+    
+    await window.ethereum.enable();
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const add = await signer.getAddress();
+
+    let result = await $.getJSON( "http://118.27.118.39:3000/api/v1/0x427444759BB5D100CC7483E9E32bdB32DE577b06/"+ add + "/" )
+    console.log(result);
+    $('#myinfo').modal('show')
+    
+    //    window.alert("開発中です。しばらくお待ちください");
 }
 
+function explorer(){
+    //window.alert("matic")
+    ans = window.confirm("ブロックチェーンエクスプローラーを開く\n\nhttps://explorer-mainnet.maticvigil.com/tokens/" +Address + "/inventory\n\nNFTの発行状況を確認しますか？");
+        if(ans){ window.open("https://explorer-mainnet.maticvigil.com/tokens/" +Address + "/inventory"); }
+
+}
+
+function opensea(){
+    //window.alert("opensea")
+    ans = window.confirm("OpenSeaでNFTレプリカを確認する\n\nhttps://opensea.io/account\n\nOpenSeaを開き、MetaMaskを接続しますか？\n(画像の反映などには数分時間がかかることがあります)");
+        if(ans){ window.open("https://opensea.io/account"); }
+}
